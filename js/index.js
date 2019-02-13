42b4d6fb5f5727827752b27bebfdbf20e48bde26
Lambda_School_Loaner_69 <lambda_school_loaner_69@LambdaSer69sMBP.hitronhub.home>
2019-02-13 00:46:21 -0600

    half finished with the last mvp task

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@
 class Tabs {
   constructor(tabs) {
     this.tabs = tabs;
-    this.btns = this.tabs.querySelector('.btns').querySelectorAll('.btn');
+    this.btns = this.tabs.querySelectorAll('.btn');
     this.btns.forEach(btn => new Button(btn));
-    this.current = document.querySelector('.show');
+    this.current = this.tabs.querySelector('.show');
   }
 }
 
@@ -12,7 +12,7 @@ class Button {
   constructor(btn) {
     this.btn = btn;
     this.data = this.btn.dataset.tab;
-    this.card = document.querySelector('.cards').querySelector(`.card[data-tab='${this.data}']`);
+    this.card = document.querySelector(`.card[data-tab='${this.data}']`);
     this.card.classList.add('hidden');
     this.card = new Card(this.card);
 
@@ -26,7 +26,6 @@ class Card {
   }
 
   show() {
-    console.log(this.card);
     serviceTabs.current.classList.remove('show');
     serviceTabs.current.classList.add('hidden');
     this.card.classList.remove('hidden');
@@ -36,3 +35,20 @@ class Card {
 }
 
 const serviceTabs = new Tabs(document.querySelector('.services-tabs'));
+
+class Menu {
+  constructor(head) {
+    this.head = head;
+    this.main = document.querySelector('main');
+    this.navBtn = this.head.querySelector('.btn');
+    this.card = this.head.querySelector('.card');
+    this.navBtn.addEventListener('click', () => this.toggle());
+  }
+
+  toggle() {
+    this.card.classList.toggle('show');
+    this.body.style.opacity = '0';
+  }
+}
+
+const menu = new Menu(document.querySelector('header'));
