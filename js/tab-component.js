8885de0d713588d813fb3e561ae8aa6cbf3def9b
Lambda_School_Loaner_69 <douglasjordan2@gmail.com>
2019-02-17 17:54:27 -0600

    animated the navbar using gsap

--- js/tab-component.js
+++ js/tab-component.js
@@ -13,22 +13,16 @@ class Button {
     this.btn = btn;
     this.data = this.btn.dataset.tab;
     this.card = document.querySelector(`.card[data-tab='${this.data}']`);
-    if(this.data !== 'nav') {
-      this.card.classList.add('hidden');
-    }
+    this.card.classList.add('hidden');
     this.card = new Card(this.card);
 
     this.btn.addEventListener('click', () => this.show())
   }
 
   show() {
-    if(this.data == 'nav') {
-      this.card.showNav();
-    } else {
-      tabs.btns.forEach(btn => btn.style.backgroundColor = 'white');
-      this.btn.style.backgroundColor = '#98A4AF'
-      this.card.show();
-    }
+    tabs.btns.forEach(btn => btn.style.backgroundColor = 'white');
+    this.btn.style.backgroundColor = '#98A4AF'
+    this.card.show();
   }
 }
 
@@ -44,19 +38,6 @@ class Card {
     this.card.classList.add('show');
     tabs.current = this.card;
   }
-
-  showNav() {
-    this.card.classList.toggle('show-nav');
-    document.querySelector('.header-top').classList.toggle('hidden');
-  }
-}
-
-const menu = new Tabs(document.querySelector('header'));
-
-const hide = () => {
-  const head = document.querySelector('header');
-  head.querySelector('.header-top').classList.toggle('hidden');
-  head.querySelector('.card').classList.toggle('show-nav');
 }
 
 const tabs = new Tabs(document.querySelector('.tabs'));
